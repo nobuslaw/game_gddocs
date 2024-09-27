c1b30bcde25ee67f725093f9b6f87c9293f7d465
Nobus <nobuslaw@users.noreply.github.com>
2024-09-27 15:59:29 +0200

    Update player.gd
    Change and add animation walk/up. Add that enemy can kill us

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,5 @@
-extends Area2D
+extends Area2D 
+signal hit
 
 @export var speed:int = 400
 var screen_size
@@ -7,22 +8,45 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	#hide()
 
+func start(pos):
+	position = pos 
+	show()
+	$CollisionShape2D.disabled = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO
-	if Input.is_action_just_pressed("move_right"):
+	
+	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	if Input.is_action_just_pressed("move_left"):
+	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-	if Input.is_action_just_pressed("move_down"):
+	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
-	if Input.is_action_just_pressed("move_up"):
+	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 	
 	if velocity.length() > 0:
-		velocity.normalized() * speed
+		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.y != 0:
+		$AnimatedSprite2D.animation = "Up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+	elif velocity.x != 0:
+		$AnimatedSprite2D.animation = "Walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0 
+
+func _on_body_entered(body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
